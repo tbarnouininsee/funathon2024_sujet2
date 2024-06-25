aff1b7573ca434bf7359d5dab0c8b77c75044c35
tbarnouininsee <timothee.barnouin@insee.fr>
2024-06-25 14:51:20 +0000

    Fonction tableau interactif

---
+++ R/tables.R
@@ -0,0 +1,20 @@
+create_table_airports <- function(df){
+  df2 <- df %>% select(-apt, -apt_nom)
+  tableau_interactif <- gt(df2) %>%
+  fmt_number(decimals = 0, suffixing = TRUE) %>%
+  fmt_markdown(columns = name_clean) %>%
+  cols_label(
+    name_clean = "Nom",
+    depart = "Départs",
+    arrivee = "Arrivées",
+    transit = "Transits",
+    total = "Total"
+  ) %>%
+  tab_header(
+    title = md("**Statistiques de fréquentation**"),
+    subtitle = md("Classement des aéroports")
+  ) %>%
+  tab_source_note(source_note = md("_Source: DGAC, à partir des données sur data.gouv.fr_")) %>% 
+  opt_interactive()
+  return(tableau_interactif)
+}
