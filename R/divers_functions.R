643b24f71557d397d05aa7817f10a75177ff8876
aurelien <aurelien.durand@insee.fr>
2024-06-25 14:18:42 +0000

    divers_functions

--- R/divers_functions.R
+++ R/divers_functions.R
@@ -5,3 +5,13 @@ create_data_from_input <- function(df, a, m){
   return(sortie)
 }
 
+summary_stat_airport <- function(df) {
+  df2 <- df %>% 
+    group_by(apt, apt_nom) %>% 
+    summarise(depart = sum(apt_pax_dep),
+              arrivee = sum(apt_pax_arr),
+              transit = sum(apt_pax_tr),
+              total = depart + arrivee + transit) %>% 
+    ungroup() %>% 
+    arrange(-total)
+  return(df2)
