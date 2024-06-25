d2a5dfc08e320e1ee76133de8208b488a2c50e18
ChasukEta <charlelie.besnard@insee.fr>
2024-06-25 08:45:07 +0000

    Commentaire fonction

--- R/create_data_list.R
+++ R/create_data_list.R
@@ -1,1 +1,9 @@
-create_data_list <- function(sources.file) yaml::read_yaml(sources.file)
+#' Création de liste
+#'
+#' @param sources.file 
+#'
+#' @return 1 liste
+#' @export
+#'
+#' @examples create_data_list("sources.yml")
+create_data_list <- function(sources.file) {yaml::read_yaml(sources.file)}
