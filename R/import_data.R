2dbde7e294b06d169ffd0ed7282da1badbd6f11f
tbarnouininsee <timothee.barnouin@insee.fr>
2024-06-25 10:15:32 +0000

    Rassemblement des fonctions d'import

---
+++ R/import_data.R
@@ -0,0 +1,20 @@
+import_airport_data <- function(list_files, ...){
+  
+  pax_apt_all <- readr::read_csv2(
+    list_files, 
+    col_types = cols(
+      ANMOIS = col_character(),
+      APT = col_character(),
+      APT_NOM = col_character(),
+      APT_ZON = col_character(),
+      .default = col_double()
+    )
+  ) %>% 
+    clean_dataframe(...)
+  
+  return(pax_apt_all)
+  
+}
+
+
+
