8ca74869220711912f0e68b8a22d23d7b4cf946d
ChasukEta <charlelie.besnard@insee.fr>
2024-06-26 11:39:19 +0000

    Démarrage application

---
+++ R/global.R
@@ -0,0 +1,48 @@
+
+# pACKAGES
+library(readr)
+library(dplyr)
+library(stringr)
+library(sf)
+library(plotly)
+library(ggplot2)
+library(gt)
+library(leaflet)
+
+# Load data ----------------------------------
+
+source("R/create_data_list.R")
+source("R/import_data.R")  
+source("R/clean_dataframe (var_temps).R")
+source("R/figures.R")
+source("R/divers_functions.R")
+source("R/tables.R")
+
+urls <- create_data_list("./sources.yml")
+
+
+airport <- import_airport_data(unlist(urls$airports))
+compagnies <- import_compagnies_data(unlist(urls$compagnies))
+liaisons <- import_liaisons_data(unlist(urls$liaisons))
+
+airports_location <- st_read(urls$geojson$airport)
+
+liste_aeroports <- unique(airport$apt)
+default_airport <- liste_aeroports[1]
+
+YEARS_LIST  <- as.character(2018:2022)
+MONTHS_LIST <- 1:12
+mois <- sample(MONTHS_LIST,1) 
+an <- sample(YEARS_LIST, 1)
+
+trafic_aeroports <- airport %>%
+  filter(apt %in% default_airport) %>%
+  mutate(
+    date = as.Date(paste(anmois, "01", sep=""), format = "%Y%m%d")
+  )
+
+
+
+
+
+
