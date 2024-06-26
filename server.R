7b02c272d144dc0b66e6eb8dc30c05581b05d38a
ChasukEta <charlelie.besnard@insee.fr>
2024-06-26 12:03:36 +0000

    maj server selon correction

--- server.R
+++ server.R
@@ -1,7 +1,30 @@
 function(input, output) {
   
-  output$date1 <- renderText(input$date)
-  output$date2 <- renderText(input$date)
-  output$airport <- renderText(input$select)
+  table_liaisons = reactive({
+    return(
+      summary_stat_airport(
+        create_data_from_input(
+          pax_apt_all,
+          year(input$date),
+          month(input$date)
+        )
+      )
+    )
+  })
+  
+  output$table <- render_gt(
+    create_table_airports(table_liaisons())
+  )
+  
+  output$carte <- renderLeaflet(
+    map_leaflet_airport(
+      pax_apt_all, airports_location,
+      month(input$date), year(input$date)
+    )
+  )
+  
+  output$lineplot <- renderPlotly(
+    plot_airport_line(pax_apt_all, input$select)
+  )
   
 }
